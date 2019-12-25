6193e9b6eba068be55d3cd53461cdf4cbfcd63c8
Elman Badalov <40571118+thebadalov@users.noreply.github.com>
2019-12-25 12:22:35 +0300

    Add files via upload

---
+++ Weather api in Adana/app.js
@@ -0,0 +1,67 @@
+
+// I wrote new elements for using JS methods in html codes.
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notification");
+const dateElement = document.querySelector(".date p");
+
+// I defined weather like array and I coded all information inside it.
+const weather = {};
+
+weather.temperature = {
+    unit : "celsius"
+}
+
+// I will use Kelvin in method.
+const KELVIN = 273;
+
+// I got api and sent to data. In next step, I used all info from json of data.
+    let api = `http://api.openweathermap.org/data/2.5/weather?q=Adana&appid=44ac2e637ba060dea8ec9f37e879086c`;
+    fetch(api)
+        .then(function(response){
+            let data = response.json();
+            return data;
+        })
+        .then(function(data){
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN); //I found normal temprature by using data.main.temp - Kelvin 
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function(){
+            displayWeather();
+        });
+
+
+// I displayed in html codes.
+function displayWeather(){
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
+
+// C to F conversion
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9/5) + 32;
+}
+
+// When you clicked temperature C it will convert to F.
+tempElement.addEventListener("click", function(){
+    if(weather.temperature.value === undefined) return;
+    
+    if(weather.temperature.unit == "celsius"){
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+        
+        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+        weather.temperature.unit = "fahrenheit";
+    }else{
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+        weather.temperature.unit = "celsius"
+    }
+});
+
